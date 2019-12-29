e7916c4ab3768d0cb369bfa769e8222abc66cad3
Tatsunomiya <black.r.d@icloud.com>
2019-12-29 22:43:01 +0900

    Initial commit

---
+++ app/src/main/java/tatsunomiya/com/qaaapp2/Const.kt
@@ -0,0 +1,8 @@
+package tatsunomiya.com.qaaapp2
+
+
+    const val UsersPATH = "users"
+
+    const val ContentsPATH = "contents"
+    const val AnswersPATH = "answers"
+    const val NameKEY = "name"
